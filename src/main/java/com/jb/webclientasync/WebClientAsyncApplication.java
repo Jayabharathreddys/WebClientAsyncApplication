dcb7d225c642abb833aca61d9171a2313b005028
Jayabharatha Reddy Sadanagiri <Jayabharathreddys@gmail.com>
2024-02-01 23:11:10 +0530

    next changes

--- src/main/java/com/jb/webclientasync/WebClientAsyncApplication.java
+++ src/main/java/com/jb/webclientasync/WebClientAsyncApplication.java
@@ -1,13 +1,18 @@
 package com.jb.webclientasync;
 
+import com.jb.webclientasync.service.QuoteService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class WebClientAsyncApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(WebClientAsyncApplication.class, args);
+
+        ConfigurableApplicationContext run = SpringApplication.run(WebClientAsyncApplication.class, args);
+        QuoteService bean = run.getBean(QuoteService.class);
+        bean.getQuoteV2();
     }
 
 }
--- src/main/java/com/jb/webclientasync/service/QuoteService.java
+++ src/main/java/com/jb/webclientasync/service/QuoteService.java
@@ -9,14 +9,31 @@ public class QuoteService {
 
     private static final String END_POINT = "https://type.fit/api/quotes";
 
-    public void getQuote(){
+    public void getQuoteV2(){
+        System.out.println("REST V2 Api call started.");
+        WebClient webClient = WebClient.create();
+        webClient.get()
+                .uri(END_POINT)
+                .retrieve()
+                .bodyToMono(String.class)
+                .subscribe(QuoteService::handleResponse);
+       // System.out.println(client.block());
+        System.out.println("REST V2 Api call got completed.");
+
+    }
+
+    public static void handleResponse(String s){
+        System.out.println(s);
+    }
+    public void getQuoteV1(){
+        System.out.println("REST V1 Api call started.");
         WebClient webClient = WebClient.create();
         Mono<String> client =  webClient.get()
                 .uri(END_POINT)
                 .retrieve()
                 .bodyToMono(String.class);
     System.out.println(client.block());
-        System.out.println("REST Api call got completed.");
+    System.out.println("REST V1 Api call got completed.");
 
     }
 }
